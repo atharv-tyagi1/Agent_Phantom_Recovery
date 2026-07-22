13bc217fa05b217815a6343e5304a65c9b10e1d1
atharv-tyagi1 <atharv-tyagi1@users.noreply.github.com>
2026-07-22 22:24:43 +0530

    feat(api): export route handlers

--- services/api/api/routes/__init__.py
+++ services/api/api/routes/__init__.py
@@ -3,5 +3,10 @@ from api.routes.projects import router as projects_router
 from api.routes.repositories import router as repositories_router
 from api.routes.tasks import router as tasks_router
 from api.routes.executions import router as executions_router
+from api.routes.repo_intel import router as repo_intel_router
 
-__all__ = ["auth_router", "projects_router", "repositories_router", "tasks_router", "executions_router"]
+__all__ = [
+    "auth_router", "projects_router", "repositories_router",
+    "tasks_router", "executions_router", "repo_intel_router"
+]
+
